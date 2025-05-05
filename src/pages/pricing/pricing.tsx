cced17628d9b68ac7e945fdf57f7b90b076b760d
dohun08 <bemik624@gmail.com>
2025-05-06 00:16:25 +0900

    fit :: 위에 고정

--- src/pages/pricing/pricing.tsx
+++ src/pages/pricing/pricing.tsx
@@ -4,8 +4,12 @@ import PricingHead from "../../features/pricing/head";
 import Faq from "../../features/pricing/faq";
 import Benefit from "../../features/pricing/benefit";
 import PricingBox from "../../features/pricing/pricing";
+import {useEffect} from "react";
 
 export default function Pricing() {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
     return (
         <Layout>
             <S.Container>
